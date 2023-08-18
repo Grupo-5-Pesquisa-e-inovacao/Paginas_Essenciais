1196717c051c6b22b8440555e3f418481cf36e5c
jhonathaGC <gabrielljjhonatha@gmail.com>
2023-08-18 18:18:49 -0300

    Inicio @jhonathaGC

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburguer = document.querySelector(".navigation");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
