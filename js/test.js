193b06938ad7868663d0dfa70caa0cd616882c29
arthursilemanali <arthur.salomaoneto@sptech.school>
2023-08-31 21:25:49 -0300

    @arthursilemanali - att responsividade

--- js/test.js
+++ js/test.js
@@ -10,12 +10,17 @@ hamburguer.addEventListener("click", () => {
 var Entrar = document.querySelector('#entrar')
 var Cadastro = document.querySelector('#cadastro')
 var btnColor = document.querySelector('.button-cor')
+var container = document.querySelector('.container');
+
+
+
 
 document.querySelector('#btnSignin')
   .addEventListener('click', () => {
     Entrar.style.left = "25px"
     Cadastro.style.left = "450px"
     btnColor.style.left = "0px"
+    container.style.height = "60vh"
 })
 
 document.querySelector('#btnSignup')
@@ -23,4 +28,27 @@ document.querySelector('#btnSignup')
     Entrar.style.left = "-450px"
     Cadastro.style.left = "25px"
     btnColor.style.left = "110px"
+    container.style.height = "90vh"
+  
+    
 })
+
+
+
+
+if (window.innerWidth <= 768) {
+  document.querySelector('#btnSignin')
+  .addEventListener('click', () => {
+    window.location.reload;
+    container.style.height = "30vh"
+})
+
+
+document.querySelector('#btnSignup')
+.addEventListener('click', () => {
+  container.style.height = "52vh"
+  window.location.reload;
+})
+}
+
+
