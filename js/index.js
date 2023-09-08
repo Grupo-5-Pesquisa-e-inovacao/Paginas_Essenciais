47345e08af7bf2c0e15e04bc7ac6864593938f49
JohnVSCordeiro <joao.cordeiro@sptech.school>
2023-09-08 16:52:47 -0300

    @johnVSCordeiro subido versão de backup do site

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const hamburguer = document.querySelector(".navigation");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburguer.addEventListener("click", () => {
-    hamburguer.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
-
